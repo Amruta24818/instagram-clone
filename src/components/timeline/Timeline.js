dcf99035f054fba68cb498e76ef707eb4c0834e9
Amruta24818 <patilamrutaj@gmail.com>
2023-10-10 15:29:42 +0530

    added dummy data for post

--- src/components/timeline/Timeline.js
+++ src/components/timeline/Timeline.js
@@ -1,15 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Timeline.css";
 import Suggestion from "./Suggestion";
 import Post from "./posts/Post";
 
 function Timeline() {
+  const [posts, setPosts] = useState([
+    {
+      user: "shubham_Jadhav",
+      postImage:
+        "https://media.istockphoto.com/id/1322277517/photo/wild-grass-in-the-mountains-at-sunset.jpg?s=612x612&w=0&k=20&c=6mItwwFFGqKNKEAzv0mv6TaxhLN3zSE43bWmFN--J5w=",
+      likes: 12,
+      timestamp: "2d",
+    },
+    {
+      user: "__amupatil__",
+      postImage:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRCIq1WFIqckI69eGZ67ugLdfxchy96eLR7w&usqp=CAU",
+      likes: 122,
+      timestamp: "1d",
+    },
+    {
+      user: "ravina_",
+      postImage:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqQMwq_mZ2I9qpXPhmIeJ5on2jZTavrF65Kw&usqp=CAU",
+      likes: 12,
+      timestamp: "2h",
+    },
+    {
+      user: "anita",
+      postImage:
+        "https://h5p.org/sites/default/files/h5p/content/1209180/images/file-6113d5f8845dc.jpeg",
+      likes: 12,
+      timestamp: "3d",
+    },
+  ]);
   return (
     <div className="timeline">
-      <div className="timeline__left"> 
-      <div className="timeline__posts">
-        <Post/>
-      </div>
+      <div className="timeline__left">
+        <div className="timeline__posts">
+          {posts.map((post) => (
+            <Post
+              user={post.user}
+              postImage={post.postImage}
+              like={post.likes}
+              timestamp={post.timestamp}
+            />
+          ))}
+        </div>
       </div>
       <div className="timeline__right">
         <Suggestion />
